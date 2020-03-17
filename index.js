425968398892d472ec64022983ac09cfdedf55f1
emily <mle4d@me.com>
2020-03-17 11:02:12 -0700

    more toggle classes

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ function logText(e) {
 divs.forEach(div => div.addEventListener('click', function() {
   document.querySelector('.one').classList.toggle('four');
   document.querySelector('.three').classList.toggle('five');
+  document.querySelector('.five').classList.toggle('six');
+  document.querySelector('.six').classList.toggle('four');
+  document.querySelector('.four').classList.toggle('seven');
+  document.querySelector('.two').classList.toggle('eight');
+  document.querySelector('.eight').classList.toggle('nine');
+  document.querySelector('.seven').classList.toggle('ten');
+  document.querySelector('.nine').classList.toggle('eleven');
 }));
 
 divs.addEventListener('click', () => {

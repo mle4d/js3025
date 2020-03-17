23918a03daeeb1cc9419a5496900cef722fd4e6a
emily <mle4d@me.com>
2020-03-17 11:07:32 -0700

    finish

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ function logText(e) {
 divs.forEach(div => div.addEventListener('click', function() {
   document.querySelector('.one').classList.toggle('four');
   document.querySelector('.three').classList.toggle('five');
+  document.querySelector('.two').classList.toggle('four');
   document.querySelector('.five').classList.toggle('six');
-  document.querySelector('.six').classList.toggle('four');
   document.querySelector('.four').classList.toggle('seven');
-  document.querySelector('.two').classList.toggle('eight');
+  document.querySelector('.six').classList.toggle('eight');
   document.querySelector('.eight').classList.toggle('nine');
   document.querySelector('.seven').classList.toggle('ten');
   document.querySelector('.nine').classList.toggle('eleven');
